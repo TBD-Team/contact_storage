32e824a26c3aef1d36703e65dfde87347029d204
Francisco Neto <fvcneto.master98@gmail.com>
2020-05-24 12:23:32 -0300

    add tesseract-ocr in the logic of the code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 from PIL import Image
 
 from settings import *
+from utils import get_string_from_image
 
 app = Flask(__name__, template_folder='template')
 
@@ -16,9 +17,9 @@ def load_image(name=None):
 
 @app.route('/post_image', methods=['GET','POST'])
 def send_image():
-    image = request.files
-    print(image, type(image))
-    return 'Hello world!'
+    request_image = request.files['imagefile'].stream
+    img = Image.open(request_image)
+    return get_string_from_image(img)
 
 if __name__ == '__main__':
     app.run(debug=DEBUG)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-import pytesseract as ocr
+import pytesseract.pytesseract as pt
+
+pt.tesseract_cmd = r'Tesseract-OCR/tesseract.exe'
 
 def get_string_from_image(img):
-    return ocr.image_to_string(img)
+    return pt.image_to_string(img)
